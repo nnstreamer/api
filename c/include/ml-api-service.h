e0db91f36c4a3aacba54288c6786bafd8e98ed9a
MyungJoo Ham <myungjoo.ham@samsung.com>
2021-11-03 12:36:36 +0900

    C/Service: introduction of the new API set.
    Declaration of what the "ML Service APIs" will provide.
    
    This is the start of designing https://github.com/nnstreamer/nnstreamer/issues/3473
    
    Signed-off-by: MyungJoo Ham <myungjoo.ham@samsung.com>

---
+++ c/include/ml-api-service.h
@@ -0,0 +1,34 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/**
+ * NNStreamer / Tizen Machine-Learning "Service API" Header
+ * Copyright (C) 2021 MyungJoo Ham <myungjoo.ham@samsung.com>
+ */
+/**
+ * @file    ml-api-service.h
+ * @date    03 Nov 2021
+ * @brief   ML-API Service Header
+ * @see     https://github.com/nnstreamer/nnstreamer
+ * @author  MyungJoo Ham <myungjoo.ham@samsung.com>
+ * @bug     No known bugs except for NYI items
+ *
+ * @details
+ *    Phase 1. With ML Service APIs, users may register intelligence pipelines
+ *      for other processes and fetch such registered pipelines.
+ *    Phase 2. With ML Service APIs, users may register/start services of
+ *      such pipelines so that other processes may share the instances of
+ *      pipelines.
+ *    Note that this requires a shared pipeline repository (Phase 1) and
+ *    a shared service/process (Phase 2), which may heavily depend on the
+ *    given software platforms or operating systems.
+ */
+#ifndef __ML_API_SERVICE_H__
+#define __ML_API_SERVICE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif /* __ML_API_SERVICE_H__ */
