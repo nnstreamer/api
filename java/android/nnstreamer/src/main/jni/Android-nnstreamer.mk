f89458d2500bdabcbd57766a08835cea1924ef03
Yongjoo Ahn <yongjoo1.ahn@samsung.com>
2021-11-15 18:55:23 +0900

    [android/mqtt] Add MQTT support
    - Add MQTT support
    - It requires paho.mqtt.c prebuilt lib
    
    Signed-off-by: Yongjoo Ahn <yongjoo1.ahn@samsung.com>

--- java/android/nnstreamer/src/main/jni/Android-nnstreamer.mk
+++ java/android/nnstreamer/src/main/jni/Android-nnstreamer.mk
@@ -37,15 +37,51 @@ NNSTREAMER_SRC_FILES += \
     $(NNSTREAMER_DECODER_PE_SRCS) \
     $(NNSTREAMER_DECODER_IS_SRCS) \
     $(NNSTREAMER_JOIN_SRCS)
+
+# TODO: Add nnsquery prebuilt-lib and enable mqtt-hybrid
+ifeq ($(ENABLE_MQTT), true)
+NNSTREAMER_SRC_FILES += \
+    $(NNSTREAMER_MQTT_SRCS)
+
+NNS_API_FLAGS += -DENABLE_MQTT=1
+
+PAHO_MQTT_C_DIR := $(LOCAL_PATH)/paho-mqtt-c
+PAHO_MQTT_C_INCLUDES := $(PAHO_MQTT_C_DIR)/include
+
+ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
+PAHO_MQTT_C_LIB_PATH := $(PAHO_MQTT_C_DIR)/lib
+else
+$(error For MQTT, target arch ABI not supported: $(TARGET_ARCH_ABI))
 endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := paho-mqtt3a
+LOCAL_SRC_FILES := $(PAHO_MQTT_C_LIB_PATH)/libpaho-mqtt3a.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := paho-mqtt3c
+LOCAL_SRC_FILES := $(PAHO_MQTT_C_LIB_PATH)/libpaho-mqtt3c.a
+include $(PREBUILT_STATIC_LIBRARY)
+endif # ifeq ($(ENABLE_MQTT), true)
+
+endif # ifneq ($(NNSTREAMER_API_OPTION),single)
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := nnstreamer
 LOCAL_SRC_FILES := $(sort $(NNSTREAMER_SRC_FILES))
 LOCAL_C_INCLUDES := $(NNSTREAMER_INCLUDES) $(GST_HEADERS_COMMON) $(NNSTREAMER_CAPI_INCLUDES)
+ifeq ($(ENABLE_MQTT), true)
+LOCAL_C_INCLUDES += $(PAHO_MQTT_C_INCLUDES)
+endif
+
 LOCAL_EXPORT_C_INCLUDES := $(NNSTREAMER_CAPI_INCLUDES)
 LOCAL_CFLAGS := -O3 -fPIC $(NNS_API_FLAGS)
 LOCAL_CXXFLAGS := -O3 -fPIC -frtti -fexceptions $(NNS_API_FLAGS)
+
+ifeq ($(ENABLE_MQTT), true)
+LOCAL_STATIC_LIBRARIES := paho-mqtt3a paho-mqtt3c
+endif
 
 include $(BUILD_STATIC_LIBRARY)
