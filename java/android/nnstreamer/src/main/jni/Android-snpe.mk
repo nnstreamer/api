917dc46f8091e05b7d536c699474fd14916fc26b
Yongjoo Ahn <yongjoo1.ahn@samsung.com>
2021-12-08 16:27:10 +0900

    [build/android] Add comments for nnfw and snpe makefiles about external libs
    - Add comments for how external libs should be configured.
    
    Signed-off-by: Yongjoo Ahn <yongjoo1.ahn@samsung.com>

--- java/android/nnstreamer/src/main/jni/Android-snpe.mk
+++ java/android/nnstreamer/src/main/jni/Android-snpe.mk
@@ -4,6 +4,10 @@
 # This mk file defines snpe module with prebuilt shared library.
 # (snpe-sdk, arm64-v8a only)
 # See Qualcomm Neural Processing SDK for AI (https://developer.qualcomm.com/software/qualcomm-neural-processing-sdk) for the details.
+#
+# You should check your `gradle.properties` to set the variable `SNPE_EXT_LIBRARY_PATH` properly.
+# The variable should be assigend with path for external shared libs.
+# An example: "SNPE_EXT_LIBRARY_PATH=src/main/jni/snpe/lib/ext"
 #------------------------------------------------------
 LOCAL_PATH := $(call my-dir)
 
