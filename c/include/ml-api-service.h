1b543f05baa23719a16dd7d231cd7fb32fce1c34
Jaeyun <jy1210.jung@samsung.com>
2022-04-11 14:25:51 +0900

    [C-Api/Service] update API description
    Under the API review, update API description and example code.
    
    Signed-off-by: Jaeyun <jy1210.jung@samsung.com>

--- c/include/ml-api-service.h
+++ c/include/ml-api-service.h
@@ -38,27 +38,63 @@ extern "C" {
 /**
  * @brief Sets the pipeline description with a given name.
  * @since_tizen 7.0
+ * @remarks If the name already exists, the pipeline description is overwritten.
  * @param[in] name Unique name to retrieve the associated pipeline description.
- * @param[in] pipeline_desc The pipeline description to be stored
+ * @param[in] pipeline_desc The pipeline description to be stored.
  * @return @c 0 on success. Otherwise a negative error value.
- * @note If the name already exists, the pipeline description is overwritten.
  * @retval #ML_ERROR_NONE Successful.
  * @retval #ML_ERROR_NOT_SUPPORTED Not supported.
  * @retval #ML_ERROR_INVALID_PARAMETER Fail. The parameter is invalid.
- * @retval #ML_ERROR_IO_ERROR The operation of DB or filesystem is failed.
+ * @retval #ML_ERROR_IO_ERROR The operation of DB or filesystem has failed.
+ *
+ * Here is an example of the usage:
+ * @code
+ * const gchar my_pipeline[] = "videotestsrc is-live=true ! videoconvert ! tensor_converter ! tensor_sink async=false";
+ * gchar *pipeline;
+ * int status;
+ * ml_pipeline_h handle;
+ *
+ * // Set pipeline description.
+ * status = ml_service_set_pipeline ("my_pipeline", my_pipeline);
+ * if (status != ML_ERROR_NONE) {
+ *   // handle error case
+ *   goto error;
+ * }
+ *
+ * // Example to construct a pipeline with stored pipeline description.
+ * // Users may register intelligence pipelines for other processes and fetch such registered pipelines.
+ * // For example, a developer adds a pipeline which includes preprocessing and invoking a neural network model,
+ * // then an application can fetch and construct this for intelligence service.
+ * status = ml_service_get_pipeline ("my_pipeline", &pipeline);
+ * if (status != ML_ERROR_NONE) {
+ *   // handle error case
+ *   goto error;
+ * }
+ *
+ * status = ml_pipeline_construct (pipeline, NULL, NULL, &handle);
+ * if (status != ML_ERROR_NONE) {
+ *   // handle error case
+ *   goto error;
+ * }
+ *
+ * error:
+ * ml_pipeline_destroy (handle);
+ * g_free (pipeline);
+ * @endcode
  */
-int ml_service_set_pipeline (const char *name, const char * pipeline_desc);
+int ml_service_set_pipeline (const char *name, const char *pipeline_desc);
 
 /**
  * @brief Gets the pipeline description with a given name.
  * @since_tizen 7.0
+ * @remarks If the function succeeds, @a pipeline_desc must be released using g_free().
  * @param[in] name The unique name to retrieve.
  * @param[out] pipeline_desc The pipeline corresponding with the given name.
  * @return @c 0 on success. Otherwise a negative error value.
  * @retval #ML_ERROR_NONE Successful.
  * @retval #ML_ERROR_NOT_SUPPORTED Not supported.
  * @retval #ML_ERROR_INVALID_PARAMETER Fail. The parameter is invalid.
- * @retval #ML_ERROR_IO_ERROR The operation of DB or filesystem is failed.
+ * @retval #ML_ERROR_IO_ERROR The operation of DB or filesystem has failed.
  */
 int ml_service_get_pipeline (const char *name, char **pipeline_desc);
 
@@ -67,11 +103,11 @@ int ml_service_get_pipeline (const char *name, char **pipeline_desc);
  * @since_tizen 7.0
  * @param[in] name The unique name to delete.
  * @return @c 0 on success. Otherwise a negative error value.
- * @note If the name did not exist in the database, this function returns ML_ERROR_NONE without any errors.
+ * @note If the name does not exist in the database, this function returns ML_ERROR_NONE without any errors.
  * @retval #ML_ERROR_NONE Successful.
  * @retval #ML_ERROR_NOT_SUPPORTED Not supported.
  * @retval #ML_ERROR_INVALID_PARAMETER Fail. The parameter is invalid.
- * @retval #ML_ERROR_IO_ERROR The operation of DB or filesystem is failed.
+ * @retval #ML_ERROR_IO_ERROR The operation of DB or filesystem has failed.
  */
 int ml_service_delete_pipeline (const char *name);
 
