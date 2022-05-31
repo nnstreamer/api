bce164f3cb3035f5b313692911eabefa4e84ac7d
Jaeyun <jy1210.jung@samsung.com>
2022-05-31 11:58:29 +0900

    [C-Api] update API description
    Prepare ACR, review native API and update description.
    
    Signed-off-by: Jaeyun <jy1210.jung@samsung.com>

--- c/include/nnstreamer.h
+++ c/include/nnstreamer.h
@@ -655,6 +655,7 @@ int ml_pipeline_element_set_property_string (ml_pipeline_element_h elem_h, const
  * @param[in] elem_h The target element handle.
  * @param[in] property_name The name of the property.
  * @param[in] value The integer value to be set.
+ * @return @c 0 on success. Otherwise a negative error value.
  * @retval #ML_ERROR_NONE Successful.
  * @retval #ML_ERROR_NOT_SUPPORTED Not supported.
  * @retval #ML_ERROR_INVALID_PARAMETER Given property name does not exist or the type is not integer.
@@ -667,6 +668,7 @@ int ml_pipeline_element_set_property_int32 (ml_pipeline_element_h elem_h, const 
  * @param[in] elem_h The target element handle.
  * @param[in] property_name The name of the property.
  * @param[in] value The integer value to be set.
+ * @return @c 0 on success. Otherwise a negative error value.
  * @retval #ML_ERROR_NONE Successful.
  * @retval #ML_ERROR_NOT_SUPPORTED Not supported.
  * @retval #ML_ERROR_INVALID_PARAMETER Given property name does not exist or the type is not integer.
@@ -745,7 +747,7 @@ int ml_pipeline_element_get_property_bool (ml_pipeline_element_h elem_h, const c
  * @since_tizen 6.0
  * @param[in] elem_h The target element handle.
  * @param[in] property_name The name of the property.
- * @param[out] value The string value of given property.
+ * @param[out] value The string value of given property. The caller is responsible for freeing the value using g_free().
  * @return @c 0 on success. Otherwise a negative error value.
  * @retval #ML_ERROR_NONE Successful.
  * @retval #ML_ERROR_NOT_SUPPORTED Not supported.
