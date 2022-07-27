dc639bdfea886c413151fb59daf53a3c71e74dc5
gichan <gichan2.jang@samsung.com>
2022-07-27 13:40:10 +0900

    [Debian] Enable test on ubuntu build
     - Enable test on ubuntu build.
    
    Signed-off-by: gichan <gichan2.jang@samsung.com>

--- packaging/run_unittests.sh
+++ packaging/run_unittests.sh
@@ -3,7 +3,7 @@
 ## @file run_unittests.sh
 ## @author Jijoong Moon <jijoong.moon@samsung.com>
 ## @date 03 April 2020
-## @brief Run unit test for NNTrainer
+## @brief Run unit test for Machine learning API.
 ##
 
 ret=0
