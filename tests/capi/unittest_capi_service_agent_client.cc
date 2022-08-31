638bce566b489c1f515ba544f816966107ef35fb
Yongjoo Ahn <yongjoo1.ahn@samsung.com>
2022-08-31 21:07:53 +0900

    [service] Add unittest for query client
    - Add simple unittests for using query client APIs.
    
    Signed-off-by: Yongjoo Ahn <yongjoo1.ahn@samsung.com>

--- tests/capi/unittest_capi_service_agent_client.cc
+++ tests/capi/unittest_capi_service_agent_client.cc
@@ -11,6 +11,7 @@
 #include <gtest/gtest.h>
 #include <ml-api-internal.h>
 #include <ml-api-service.h>
+#include <ml-api-inference-pipeline-internal.h>
 
 #include <netinet/tcp.h>
 #include <netinet/in.h>
@@ -315,6 +316,173 @@ TEST_F (MLServiceAgentTest, close_pipeline_00_n)
 }
 
 /**
+ * @brief use case of using service api
+ */
+TEST_F (MLServiceAgentTest, query_client)
+{
+  int status;
+
+  /** Set server pipeline and launch it */
+  const gchar *service_name = "simple_query_server_for_test";
+  int num_buffers = 5;
+  guint server_port = _get_available_port ();
+  gchar *server_pipeline_desc = g_strdup_printf ("tensor_query_serversrc port=%u num-buffers=%d ! other/tensors,num_tensors=1,dimensions=3:4:4:1,types=uint8,format=static,framerate=0/1 ! tensor_query_serversink async=false sync=false", server_port, num_buffers);
+
+  status = ml_service_set_pipeline (service_name, server_pipeline_desc);
+  EXPECT_EQ (ML_ERROR_NONE, status);
+
+  gchar *ret_pipeline;
+  status = ml_service_get_pipeline (service_name, &ret_pipeline);
+  EXPECT_EQ (ML_ERROR_NONE, status);
+  EXPECT_STREQ (server_pipeline_desc, ret_pipeline);
+  g_free (server_pipeline_desc);
+  g_free (ret_pipeline);
+
+  ml_service_h service;
+  ml_pipeline_state_e state;
+  status = ml_service_launch_pipeline (service_name, &service);
+  EXPECT_EQ (ML_ERROR_NONE, status);
+
+  status = ml_service_get_pipeline_state (service, &state);
+  EXPECT_EQ (ML_ERROR_NONE, status);
+  EXPECT_EQ (ML_PIPELINE_STATE_PAUSED, state);
+
+  status = ml_service_start_pipeline (service);
+  EXPECT_EQ (ML_ERROR_NONE, status);
+  status = ml_service_get_pipeline_state (service, &state);
+  EXPECT_EQ (ML_ERROR_NONE, status);
+  EXPECT_EQ (ML_PIPELINE_STATE_PLAYING, state);
+
+  ml_service_h client;
+  ml_option_h query_client_option = NULL;
+
+  status = ml_option_create (&query_client_option);
+  EXPECT_EQ (ML_ERROR_NONE, status);
+
+  gchar *host = g_strdup ("localhost");
+  status = ml_option_set (query_client_option, "host", host, g_free);
+  EXPECT_EQ (ML_ERROR_NONE, status);
+
+  guint client_port = _get_available_port ();
+  status = ml_option_set (query_client_option, "port", &client_port, NULL);
+  EXPECT_EQ (ML_ERROR_NONE, status);
+
+  gchar *dest_host = g_strdup ("localhost");
+  status = ml_option_set (query_client_option, "dest-host", dest_host, g_free);
+  EXPECT_EQ (ML_ERROR_NONE, status);
+
+  guint dest_port = server_port;
+  status = ml_option_set (query_client_option, "dest-port", &dest_port, NULL);
+  EXPECT_EQ (ML_ERROR_NONE, status);
+
+  gchar *connect_type = g_strdup ("TCP");
+  status = ml_option_set (query_client_option, "connect-type", connect_type, g_free);
+  EXPECT_EQ (ML_ERROR_NONE, status);
+
+  guint timeout = 10000U;
+  status = ml_option_set (query_client_option, "timeout", &timeout, NULL);
+  EXPECT_EQ (ML_ERROR_NONE, status);
+
+  gchar *caps_str = g_strdup ("other/tensors,num_tensors=1,format=static,types=uint8,dimensions=3:4:4:1,framerate=0/1");
+  status = ml_option_set (query_client_option, "caps", caps_str, g_free);
+  EXPECT_EQ (ML_ERROR_NONE, status);
+
+  /* set input tensor */
+  ml_tensors_info_h in_info;
+  ml_tensor_dimension in_dim;
+  ml_tensors_data_h input;
+
+  ml_tensors_info_create (&in_info);
+  in_dim[0] = 3;
+  in_dim[1] = 4;
+  in_dim[2] = 4;
+  in_dim[3] = 1;
+
+  ml_tensors_info_set_count (in_info, 1);
+  ml_tensors_info_set_tensor_type (in_info, 0, ML_TENSOR_TYPE_UINT8);
+  ml_tensors_info_set_tensor_dimension (in_info, 0, in_dim);
+
+  status = ml_service_query_create (query_client_option, &client);
+  EXPECT_EQ (ML_ERROR_NONE, status);
+
+  status = ml_tensors_data_create (in_info, &input);
+  EXPECT_EQ (ML_ERROR_NONE, status);
+  EXPECT_TRUE (NULL != input);
+
+  /* request output tensor with input tensor */
+  for (int i = 0; i < num_buffers; ++i) {
+    ml_tensors_data_h output;
+    uint8_t *received;
+    size_t input_data_size, output_data_size;
+    uint8_t test_data = (uint8_t) i;
+
+    ml_tensors_data_set_tensor_data (input, 0, &test_data, sizeof (uint8_t));
+
+    status = ml_service_query_request (client, input, &output);
+    EXPECT_EQ (ML_ERROR_NONE, status);
+    EXPECT_TRUE (NULL != output);
+
+    status = ml_tensors_info_get_tensor_size (in_info, 0, &input_data_size);
+    EXPECT_EQ (ML_ERROR_NONE, status);
+
+    status = ml_tensors_data_get_tensor_data (output, 0, (void **) &received, &output_data_size);
+    EXPECT_EQ (ML_ERROR_NONE, status);
+    EXPECT_EQ (input_data_size, output_data_size);
+    EXPECT_EQ (test_data, received[0]);
+
+    status = ml_tensors_data_destroy (output);
+    EXPECT_EQ (ML_ERROR_NONE, status);
+  }
+
+  /** destroy client ml_service_h */
+  status = ml_service_destroy (client);
+  EXPECT_EQ (ML_ERROR_NONE, status);
+
+  /** destroy server pipeline */
+  status = ml_service_stop_pipeline (service);
+  EXPECT_EQ (ML_ERROR_NONE, status);
+
+  status = ml_service_get_pipeline_state (service, &state);
+  EXPECT_EQ (ML_ERROR_NONE, status);
+  EXPECT_EQ (ML_PIPELINE_STATE_PAUSED, state);
+
+  status = ml_service_destroy (service);
+  EXPECT_EQ (ML_ERROR_NONE, status);
+
+  /** delete finished service */
+  status = ml_service_delete_pipeline (service_name);
+  EXPECT_EQ (ML_ERROR_NONE, status);
+
+  /** it would fail if get the removed service */
+  status = ml_service_get_pipeline (service_name, &ret_pipeline);
+  EXPECT_EQ (ML_ERROR_INVALID_PARAMETER, status);
+
+  ml_option_destroy (query_client_option);
+  ml_tensors_data_destroy (input);
+  ml_tensors_info_destroy (in_info);
+}
+
+/**
+ * @brief Test ml_service_query_create with invalid param.
+ */
+TEST_F (MLServiceAgentTest, query_create_00_n)
+{
+  int status;
+  status = ml_service_query_create (NULL, NULL);
+  EXPECT_EQ (ML_ERROR_INVALID_PARAMETER, status);
+}
+
+/**
+ * @brief Test ml_service_query_request with invalid param.
+ */
+TEST_F (MLServiceAgentTest, query_request_00_n)
+{
+  int status;
+  status = ml_service_query_request (NULL, NULL, NULL);
+  EXPECT_EQ (ML_ERROR_INVALID_PARAMETER, status);
+}
+
+/**
  * @brief Main gtest
  */
 int
@@ -327,6 +495,8 @@ main (int argc, char **argv)
   } catch (...) {
     g_warning ("catch 'testing::internal::<unnamed>::ClassUniqueToAlwaysTrue'");
   }
+
+  _ml_initialize_gstreamer ();
 
   /* ignore tizen feature status while running the testcases */
   set_feature_state (ML_FEATURE, SUPPORTED);
