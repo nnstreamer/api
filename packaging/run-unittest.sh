fd2f196b8a1f4bb3f89f31fd7f15fc4c1786ea2f
gichan <gichan2.jang@samsung.com>
2022-12-08 16:13:51 +0900

    [Tizen] Generate packages for automation test tools
     - Generate gcov and unittest package with "gcov 1" option for tizen test
    automation tools.
    
    Signed-off-by: gichan <gichan2.jang@samsung.com>

---
+++ packaging/run-unittest.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+##
+## @file run-unittest.sh
+## @author Gichan Jang <gichan2.jang@samsung.com>
+## @date 08 Dec 2022
+## @brief Run unit test on automation tool.
+##
+setup() {
+    echo "setup start"
+}
+
+test_main() {
+    echo "test_main start"
+	testlist=$(find /usr/bin/unittest-ml -type f -executable -name "unittest_*")
+    for test in ${testlist}; do
+	  ${test}
+    done
+}
+
+teardown() {
+    echo "teardown start"
+}
+
+main() {
+    setup
+    test_main
+    teardown
+}
+
+main "\$*"
