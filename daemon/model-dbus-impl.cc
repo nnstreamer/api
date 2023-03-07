983d59a270db6185e6eb1444c5ddfc50fa797305
Yongjoo Ahn <yongjoo1.ahn@samsung.com>
2023-03-07 20:03:52 +0900

    [daemon] Implement dbus method callbacks
    - Implement callback for each dbus methods:
      register, update_description, activate,
      get, get_activated, get_all, delete
    
    Signed-off-by: Yongjoo Ahn <yongjoo1.ahn@samsung.com>

--- daemon/model-dbus-impl.cc
+++ daemon/model-dbus-impl.cc
@@ -47,13 +47,17 @@ gdbus_put_model_instance (MachinelearningServiceModel ** instance)
  * @param invoc Method invocation handle.
  * @param name The name of target model.
  * @param path The file path of target.
+ * @param is_active The active status of target model.
+ * @param description The description of target model.
  * @return @c TRUE if the request is handled. FALSE if the service is not available.
  */
 static gboolean
 gdbus_cb_model_register (MachinelearningServiceModel *obj,
     GDBusMethodInvocation *invoc,
     const gchar *name,
-    const gchar *path)
+    const gchar *path,
+    const bool is_active,
+    const gchar *description)
 {
   int ret = 0;
   guint version = 0U;
@@ -63,7 +67,8 @@ gdbus_cb_model_register (MachinelearningServiceModel *obj,
     /**
      * @todo register the given model to the DB, and get the version of the model.
     */
-   version = 1U;
+    db.connectDB ();
+    db.set_model (name, path, is_active, description, &version);
   } catch (...) {
     g_critical ("DB error occurred. Failed to register the model: %s", name);
     ret = -EIO;
@@ -76,6 +81,233 @@ gdbus_cb_model_register (MachinelearningServiceModel *obj,
 }
 
 /**
+ * @brief The callback function of update description method
+ *
+ * @param obj Proxy instance.
+ * @param invoc Method invocation handle.
+ * @param name The name of target model.
+ * @param version The version of target model.
+ * @param description The description of target model.
+ * @return @c TRUE if the request is handled. FALSE if the service is not available.
+ */
+static gboolean
+gdbus_cb_model_update_description (MachinelearningServiceModel *obj,
+    GDBusMethodInvocation *invoc,
+    const gchar *name,
+    const guint version,
+    const gchar *description)
+{
+  int ret = 0;
+  MLServiceDB & db = MLServiceDB::getInstance ();
+
+  try {
+    /**
+     * @todo update the description of the given model to the DB.
+    */
+    db.connectDB ();
+    db.update_model_description (name, version, description);
+  } catch (const std::invalid_argument &e) {
+    g_critical ("There is no such model");
+    ret = -EINVAL;
+  } catch (...) {
+    g_critical ("DB error occurred. Failed to update the model description: %s", name);
+    ret = -EIO;
+  }
+
+  db.disconnectDB ();
+  machinelearning_service_model_complete_update_description (obj, invoc, ret);
+
+  return TRUE;
+}
+
+/**
+ * @brief The callback function of activate method
+ *
+ * @param obj Proxy instance.
+ * @param invoc Method invocation handle.
+ * @param name The name of target model.
+ * @param version The version of target model.
+ * @return @c TRUE if the request is handled. FALSE if the service is not available.
+ */
+static gboolean
+gdbus_cb_model_activate (MachinelearningServiceModel *obj,
+    GDBusMethodInvocation *invoc,
+    const gchar *name,
+    const guint version)
+{
+  int ret = 0;
+  MLServiceDB & db = MLServiceDB::getInstance ();
+
+  try {
+    /**
+     * @todo activate the given model to the DB.
+    */
+    db.connectDB ();
+    db.activate_model (name, version);
+  } catch (const std::invalid_argument &e) {
+    g_critical ("There is no such model");
+    ret = -EINVAL;
+  } catch (...) {
+    g_critical ("DB error occurred. Failed to activate the model: %s", name);
+    ret = -EIO;
+  }
+
+  db.disconnectDB ();
+  machinelearning_service_model_complete_activate (obj, invoc, ret);
+
+  return TRUE;
+}
+
+/**
+ * @brief The callback function of get method
+ *
+ * @param obj Proxy instance.
+ * @param invoc Method invocation handle.
+ * @param name The name of target model.
+ * @return @c TRUE if the request is handled. FALSE if the service is not available.
+ */
+static gboolean
+gdbus_cb_model_get (MachinelearningServiceModel *obj,
+    GDBusMethodInvocation *invoc,
+    const gchar *name,
+    const guint version)
+{
+  int ret = 0;
+  std::string model_info;
+  MLServiceDB & db = MLServiceDB::getInstance ();
+
+  try {
+    db.connectDB ();
+    db.get_model (name, model_info, version);
+  } catch (const std::invalid_argument &e) {
+    g_critical ("There is no active model");
+    ret = -EINVAL;
+  } catch (...) {
+    g_critical ("DB error occurred. Failed to get the active model: %s", name);
+    ret = -EIO;
+  }
+
+  db.disconnectDB ();
+  machinelearning_service_model_complete_get_activated (obj, invoc, model_info.c_str (), ret);
+
+  return TRUE;
+}
+
+/**
+ * @brief The callback function of get activated method
+ *
+ * @param obj Proxy instance.
+ * @param invoc Method invocation handle.
+ * @param name The name of target model.
+ * @return @c TRUE if the request is handled. FALSE if the service is not available.
+ */
+static gboolean
+gdbus_cb_model_get_activated (MachinelearningServiceModel *obj,
+    GDBusMethodInvocation *invoc,
+    const gchar *name)
+{
+  int ret = 0;
+  std::string model_info;
+  MLServiceDB & db = MLServiceDB::getInstance ();
+
+  try {
+    /**
+     * @todo get the active status of the given model from the DB.
+    */
+    db.connectDB ();
+    db.get_model (name, model_info, -1);
+  } catch (const std::invalid_argument &e) {
+    g_critical ("There is no active model");
+    ret = -EINVAL;
+  } catch (...) {
+    g_critical ("DB error occurred. Failed to get the active model: %s", name);
+    ret = -EIO;
+  }
+
+  db.disconnectDB ();
+  machinelearning_service_model_complete_get_activated (obj, invoc, model_info.c_str (), ret);
+
+  return TRUE;
+}
+
+/**
+ * @brief The callback function of get all method
+ *
+ * @param obj Proxy instance.
+ * @param invoc Method invocation handle.
+ * @param name The name of target model.
+ * @return @c TRUE if the request is handled. FALSE if the service is not available.
+ */
+static gboolean
+gdbus_cb_model_get_all (MachinelearningServiceModel *obj,
+    GDBusMethodInvocation *invoc,
+    const gchar *name
+    )
+{
+  int ret = 0;
+  MLServiceDB & db = MLServiceDB::getInstance ();
+  std::string all_model_list;
+
+  try {
+    /**
+     * @todo get all the models from the DB.
+    */
+    db.connectDB ();
+    db.get_model (name, all_model_list, 0);
+  } catch (const std::invalid_argument &e) {
+    g_critical ("There is no such model");
+    ret = -EINVAL;
+  } catch (...) {
+    g_critical ("DB error occurred. Failed to get all the models");
+    ret = -EIO;
+  }
+
+  db.disconnectDB ();
+
+  machinelearning_service_model_complete_get (obj, invoc, all_model_list.c_str (), ret);
+
+  return TRUE;
+}
+
+/**
+ * @brief The callback function of delete method
+ *
+ * @param obj Proxy instance.
+ * @param invoc Method invocation handle.
+ * @param name The name of target model.
+ * @param version The version of target model.
+ * @return @c TRUE if the request is handled. FALSE if the service is not available.
+ */
+static gboolean
+gdbus_cb_model_delete (MachinelearningServiceModel *obj,
+    GDBusMethodInvocation *invoc,
+    const gchar *name,
+    const guint version)
+{
+  int ret = 0;
+  MLServiceDB & db = MLServiceDB::getInstance ();
+
+  try {
+    /**
+     * @todo delete the given model from the DB.
+    */
+    db.connectDB ();
+    db.delete_model (name, version);
+  } catch (const std::invalid_argument &e) {
+    g_critical ("There is no such model");
+    ret = -EINVAL;
+  } catch (...) {
+    g_critical ("DB error occurred. Failed to delete the model: %s", name);
+    ret = -EIO;
+  }
+
+  db.disconnectDB ();
+  machinelearning_service_model_complete_delete (obj, invoc, ret);
+
+  return TRUE;
+}
+
+/**
  * @brief Event handler list of Model interface
  */
 static struct gdbus_signal_info handler_infos[] = {
@@ -83,8 +315,38 @@ static struct gdbus_signal_info handler_infos[] = {
     .signal_name = DBUS_MODEL_I_HANDLER_REGISTER,
     .cb = G_CALLBACK (gdbus_cb_model_register),
     .cb_data = NULL,
-    .handler_id = 0,
-  },
+    .handler_id = 1U,
+  }, {
+    .signal_name = DBUS_MODEL_I_HANDLER_UPDATE_DESCRIPTION,
+    .cb = G_CALLBACK (gdbus_cb_model_update_description),
+    .cb_data = NULL,
+    .handler_id = 2U,
+  }, {
+    .signal_name = DBUS_MODEL_I_HANDLER_ACTIVATE,
+    .cb = G_CALLBACK (gdbus_cb_model_activate),
+    .cb_data = NULL,
+    .handler_id = 3U,
+  }, {
+    .signal_name = DBUS_MODEL_I_HANDLER_GET,
+    .cb = G_CALLBACK (gdbus_cb_model_get),
+    .cb_data = NULL,
+    .handler_id = 4U,
+  }, {
+    .signal_name = DBUS_MODEL_I_HANDLER_GET_ACTIVATED,
+    .cb = G_CALLBACK (gdbus_cb_model_get_activated),
+    .cb_data = NULL,
+    .handler_id = 5U,
+  }, {
+    .signal_name = DBUS_MODEL_I_HANDLER_GET_ALL,
+    .cb = G_CALLBACK (gdbus_cb_model_get_all),
+    .cb_data = NULL,
+    .handler_id = 6U,
+  }, {
+    .signal_name = DBUS_MODEL_I_HANDLER_DELETE,
+    .cb = G_CALLBACK (gdbus_cb_model_delete),
+    .cb_data = NULL,
+    .handler_id = 7U,
+  }
 };
 
 /**
