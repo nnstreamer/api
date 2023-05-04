757832936e293d3fe40b4d59dc572126ff264a20
Jaeyun Jung <jy1210.jung@samsung.com>
2023-05-04 14:16:33 +0900

    [Build/Service] fix indent
    Fix build error, clang-format of service-db.
    
    Signed-off-by: Jaeyun Jung <jy1210.jung@samsung.com>

--- daemon/includes/service-db.hh
+++ daemon/includes/service-db.hh
@@ -14,35 +14,36 @@
 #define __SERVICE_DB_HH__
 
 #include <glib.h>
-#include <sqlite3.h>
 #include <iostream>
+#include <sqlite3.h>
 
 /**
  * @brief Class for ML-Service Database.
  */
 class MLServiceDB
 {
-public:
+  public:
   MLServiceDB (const MLServiceDB &) = delete;
   MLServiceDB (MLServiceDB &&) = delete;
-  MLServiceDB & operator= (const MLServiceDB &) = delete;
-  MLServiceDB & operator= (MLServiceDB &&) = delete;
+  MLServiceDB &operator= (const MLServiceDB &) = delete;
+  MLServiceDB &operator= (MLServiceDB &&) = delete;
 
   virtual void connectDB ();
   virtual void disconnectDB ();
   virtual void set_pipeline (const std::string name, const std::string description);
   virtual void get_pipeline (const std::string name, std::string &description);
   virtual void delete_pipeline (const std::string name);
-  /** @todo update methods to handle nn model */
-  virtual void set_model (const std::string name, const std::string model, const bool is_active, const std::string description, const std::string app_info, guint *version);
-  virtual void update_model_description (const std::string name, const guint version, const std::string description);
+  virtual void set_model (const std::string name, const std::string model, const bool is_active,
+      const std::string description, const std::string app_info, guint *version);
+  virtual void update_model_description (const std::string name,
+      const guint version, const std::string description);
   virtual void activate_model (const std::string name, const guint version);
   virtual void get_model (const std::string name, std::string &model, const gint version);
   virtual void delete_model (const std::string name, const guint version);
 
-  static MLServiceDB & getInstance (void);
+  static MLServiceDB &getInstance (void);
 
-private:
+  private:
   MLServiceDB (std::string path);
   virtual ~MLServiceDB ();
 
