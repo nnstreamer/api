72362dd51808b9c222f451078c848162b380fb3d
hyunil park <hyunil46.park@samsung.com>
2024-03-06 17:34:56 +0900

    [ml-service] Add training offloading service to ml-service
    - Add function for training offloading
    - Add function to transfer trained model
    - Add node information to transmitted pipeline information
    - set sink callback by node name of node information
    - Add stop func for ready to complete model training
    - Add test case
    
    Signed-off-by: hyunil park <hyunil46.park@samsung.com>

--- c/src/ml-api-service-offloading.h
+++ c/src/ml-api-service-offloading.h
@@ -33,6 +33,36 @@ extern "C" {
 int ml_service_offloading_create (ml_service_h handle, ml_option_h option);
 
 /**
+ * @brief Start ml offloading service.
+ * @remarks The @a handle should be destroyed using ml_service_destroy().
+ * @param[in] handle ml-service handle created by ml_service_new().
+ * @return @c 0 on Success. Otherwise a negative error value.
+ * @retval #ML_ERROR_NONE Successful.
+ * @retval #ML_ERROR_NOT_SUPPORTED Not supported.
+ * @retval #ML_ERROR_INVALID_PARAMETER Fail. The parameter is invalid.
+ * @retval #ML_ERROR_OUT_OF_MEMORY Failed to allocate required memory.
+ * @retval #ML_ERROR_STREAMS_PIPE Failed to launch the pipeline.
+ * @retval #ML_ERROR_TRY_AGAIN The pipeline is not ready yet.
+ * @retval #ML_ERROR_PERMISSION_DENIED The application does not have the privilege to access to the storage.
+ */
+int ml_service_offloading_start (ml_service_h handle);
+
+/**
+ * @brief Stop ml offloading service.
+ * @remarks The @a handle should be destroyed using ml_service_destroy().
+ * @param[in] handle ml-service handle created by ml_service_new().
+ * @return @c 0 on Success. Otherwise a negative error value.
+ * @retval #ML_ERROR_NONE Successful.
+ * @retval #ML_ERROR_NOT_SUPPORTED Not supported.
+ * @retval #ML_ERROR_INVALID_PARAMETER Fail. The parameter is invalid.
+ * @retval #ML_ERROR_OUT_OF_MEMORY Failed to allocate required memory.
+ * @retval #ML_ERROR_STREAMS_PIPE Failed to launch the pipeline.
+ * @retval #ML_ERROR_TRY_AGAIN The pipeline is not ready yet.
+ * @retval #ML_ERROR_PERMISSION_DENIED The application does not have the privilege to access to the storage.
+ */
+int ml_service_offloading_stop (ml_service_h handle);
+
+/**
  * @brief Request service to ml-service offloading.
  * @param[in] handle The handle of ml-service.
  * @param[in] key The key of machine learning service.
@@ -69,15 +99,36 @@ int ml_service_offloading_set_service (ml_service_h handle, const char *name, co
 int ml_service_offloading_set_information (ml_service_h handle, const char *name, const char *value);
 
 /**
+ * @brief Set training offloading handle or Set null when destroying.
+ * @param[in] handle The handle of ml-service
+ * @param[in] training_handle training offloading handle or NULL.
+ * @return @c 0 on success. Otherwise a negative error value.
+ * @retval #ML_ERROR_NONE Successful.
+ * @retval #ML_ERROR_NOT_SUPPORTED Not supported.
+ * @retval #ML_ERROR_INVALID_PARAMETER Given parameter is invalid.
+ */
+int ml_service_offloading_set_training_handle (ml_service_h handle, void *training_handle);
+
+/**
+ * @brief Get training offloading handle.
+ * @param[in] handle The handle of ml-service.
+ */
+void *ml_service_offloading_get_training_handle (ml_service_h handle);
+
+/**
  * @brief Internal function to release ml-service offloading data.
  */
 int ml_service_offloading_release_internal (ml_service_s *mls);
 #else
 #define ml_service_offloading_create(...) ML_ERROR_NOT_SUPPORTED
+#define ml_service_offloading_start(...) ML_ERROR_NOT_SUPPORTED
+#define ml_service_offloading_stop(...) ML_ERROR_NOT_SUPPORTED
 #define ml_service_offloading_request(...) ML_ERROR_NOT_SUPPORTED
 #define ml_service_offloading_set_service(...) ML_ERROR_NOT_SUPPORTED
 #define ml_service_offloading_set_information(...) ML_ERROR_NOT_SUPPORTED
 #define ml_service_offloading_release_internal(...) ML_ERROR_NOT_SUPPORTED
+#define ml_service_offloading_set_training_handle(...) ML_ERROR_NOT_SUPPORTED
+#define ml_service_offloading_get_training_handle(...) ML_ERROR_NOT_SUPPORTED
 #endif /* ENABLE_SERVICE_OFFLOADING */
 
 #ifdef __cplusplus
---
+++ c/src/ml-api-service-training-offloading.h
@@ -0,0 +1,121 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/**
+ * Copyright (c) 2024 Samsung Electronics Co., Ltd. All Rights Reserved.
+ *
+ * @file ml-api-service-training-offloading.h
+ * @date 5 Apr 2024
+ * @brief ml-service training offloading internal header.
+ *        This file should NOT be exported to SDK or devel package.
+ * @brief ML training offloading service of NNStreamer/Service C-API
+ * @see https://github.com/nnstreamer/nnstreamer
+ * @author Hyunil Park <hyunil46.park@samsung.com>
+ * @bug No known bugs except for NYI items
+ */
+
+#ifndef __ML_SERVICE_TRAINING_OFFLOADING_H__
+#define __ML_SERVICE_TRAINING_OFFLOADING_H__
+
+#include <ml-api-service.h>
+#include "nnstreamer-tizen-internal.h"
+#include "ml-api-service-offloading.h"
+#include "ml-api-service-private.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
+/**
+ * @brief Creates a training offloading handle for ml-service training offloading service.
+ * @param[in] mls ml-service handle created by ml_service_new().
+ * @param[in] object The Json object containing the service option.
+ * @return @c 0 on success. Otherwise a negative error value.
+ * @retval #ML_ERROR_NONE Successful.
+ * @retval #ML_ERROR_NOT_SUPPORTED Not supported.
+ * @retval #ML_ERROR_PERMISSION_DENIED The application does not have the privilege to access to the media storage or external storage.
+ * @retval #ML_ERROR_INVALID_PARAMETER The parameter is invalid.
+ * @retval #ML_ERROR_IO_ERROR Failed to parse the configuration file.
+ * @retval #ML_ERROR_STREAMS_PIPE Failed to open the model.
+ * @retval #ML_ERROR_OUT_OF_MEMORY Failed to allocate required memory.
+ */
+  int ml_service_training_offloading_create (ml_service_s * mls, JsonObject * object);
+
+/**
+ * @brief Set path in ml-service training offloading handle.
+ * @note This is not official and public API but experimental API.
+ * @param[in] mls ml-service handle created by ml_service_new().
+ * @param[in] path Readable and writable path set by the app.
+ * @return @c 0 on success. Otherwise a negative error value.
+ * @retval #ML_ERROR_NONE Successful.
+ * @retval #ML_ERROR_NOT_SUPPORTED Not supported.
+ * @retval #ML_ERROR_INVALID_PARAMETER Given parameter is invalid.
+ */
+  int ml_service_training_offloading_set_path (ml_service_s *mls, const gchar * path);
+
+/**
+ * @brief Start ml training offloading service.
+ * @remarks The @a handle should be destroyed using ml_service_destroy().
+ * @param[in] mls ml-service handle created by ml_service_new().
+ * @return @c 0 on Success. Otherwise a negative error value.
+ * @retval #ML_ERROR_NONE Successful.
+ * @retval #ML_ERROR_NOT_SUPPORTED Not supported.
+ * @retval #ML_ERROR_INVALID_PARAMETER Fail. The parameter is invalid.
+ * @retval #ML_ERROR_OUT_OF_MEMORY Failed to allocate required memory.
+ * @retval #ML_ERROR_STREAMS_PIPE Failed to launch the pipeline.
+ * @retval #ML_ERROR_TRY_AGAIN The pipeline is not ready yet.
+ * @retval #ML_ERROR_PERMISSION_DENIED The application does not have the privilege to access to the storage.
+ */
+  int ml_service_training_offloading_start (ml_service_s * mls);
+
+/**
+ * @brief Stop ml training offloading service.
+ * @remarks The @a handle should be destroyed using ml_service_destroy().
+ * @param[in] mls ml-service handle created by ml_service_new().
+ * @return @c 0 on Success. Otherwise a negative error value.
+ * @retval #ML_ERROR_NONE Successful.
+ * @retval #ML_ERROR_NOT_SUPPORTED Not supported.
+ * @retval #ML_ERROR_INVALID_PARAMETER Fail. The parameter is invalid.
+ * @retval #ML_ERROR_OUT_OF_MEMORY Failed to allocate required memory.
+ * @retval #ML_ERROR_STREAMS_PIPE Failed to launch the pipeline.
+ * @retval #ML_ERROR_TRY_AGAIN The pipeline is not ready yet.
+ * @retval #ML_ERROR_PERMISSION_DENIED The application does not have the privilege to access to the storage.
+ */
+  int ml_service_training_offloading_stop (ml_service_s * mls);
+
+/**
+ * @brief Request all services to ml-service offloading.
+ * @param[in] mls ml-service handle created by ml_service_new().
+ * @return @c 0 on success. Otherwise a negative error value.
+ * @retval #ML_ERROR_NONE Successful.
+ * @retval #ML_ERROR_NOT_SUPPORTED Not supported.
+ * @retval #ML_ERROR_INVALID_PARAMETER Given parameter is invalid.
+ */
+  int ml_service_training_offloading_all_services_request (ml_service_s * mls);
+
+/**
+ * @brief Process received data
+ * @param[in] mls ml-service handle created by ml_service_new().
+ * @param[in] data_h handle nns_edge_data_h
+ * @param[in] data data of received file
+ * @param[in] dir_path dir path
+ * @param[in] service_type received service type from remote edge
+ */
+  void ml_service_training_offloading_process_received_data (ml_service_s * mls, void * data_h, const gchar *dir_path, const gchar * data, int service_type);
+
+/**
+ * @brief Internal function to destroy ml-service training offloading data.
+ * @param[in] mls ml-service handle created by ml_service_new().
+ * @return @c 0 on success. Otherwise a negative error value.
+ * @retval #ML_ERROR_NONE Successful.
+ * @retval #ML_ERROR_NOT_SUPPORTED Not supported.
+ * @retval #ML_ERROR_INVALID_PARAMETER Given parameter is invalid.
+ * @return @c 0 on success. Otherwise a negative error value.
+ * @retval #ML_ERROR_NONE Successful.
+ * @retval #ML_ERROR_NOT_SUPPORTED Not supported.
+ * @retval #ML_ERROR_INVALID_PARAMETER Given parameter is invalid.
+ */
+  int ml_service_training_offloading_destroy (ml_service_s * mls);
+#ifdef __cplusplus
+}
+#endif  /* __cplusplus */
+#endif  /* __ML_SERVICE_TRAINING_OFFLOADING_H__ */
