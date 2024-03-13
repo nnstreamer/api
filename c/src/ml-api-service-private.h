de47369c763f623ff0bba5c0900d311043366894
gichan2-jang <gichan2.jang@samsung.com>
2024-03-13 14:19:42 +0900

    [ml-service] Change node type
    Change ml-service node type from pub/sub to query server/client to send
    reply.
    
    Signed-off-by: gichan2-jang <gichan2.jang@samsung.com>

--- c/src/ml-api-service-private.h
+++ c/src/ml-api-service-private.h
@@ -31,6 +31,7 @@ extern "C" {
  */
 #define ML_SERVICE_EVENT_MODEL_REGISTERED 2
 #define ML_SERVICE_EVENT_PIPELINE_REGISTERED 3
+#define ML_SERVICE_EVENT_REPLY 4
 
 /**
  * @brief Enumeration for ml-service type.
