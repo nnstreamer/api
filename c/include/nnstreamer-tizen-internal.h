3fade27e42d95c4f9696921e72f36679912f7330
Jaeyun Jung <jy1210.jung@samsung.com>
2025-08-08 17:30:29 +0900

    [C-Api] async invoke feature
    Add new callback and options to get the inference result asynchronously.
    
    Signed-off-by: Jaeyun Jung <jy1210.jung@samsung.com>

--- c/include/nnstreamer-tizen-internal.h
+++ c/include/nnstreamer-tizen-internal.h
@@ -20,6 +20,17 @@ extern "C" {
 #endif /* __cplusplus */
 
 /**
+ * @brief Callback for tensor data stream of machine-learning API.
+ * @details Note that the buffer may be deallocated after the return and this is synchronously called. Thus, if you need the data afterwards, copy the data to another buffer and return fast. Do not spend too much time in the callback.
+ * @since_tizen 10.0
+ * @remarks The @a data can be used only in the callback. To use outside, make a copy.
+ * @param[in] data The handle of the tensor data (a single frame. tensor/tensors). You can get the information of given tensor data frame using ml_tensors_data_get_info().
+ * @param[in,out] user_data User application's private data.
+ * @return @c 0 on success. Otherwise a negative error value.
+ */
+typedef int (*ml_tensors_data_cb) (const ml_tensors_data_h data, void *user_data);
+
+/**
  * @brief Constructs the pipeline (GStreamer + NNStreamer).
  * @details This function is to construct the pipeline without checking the permission in platform internally. See ml_pipeline_construct() for the details.
  * @since_tizen 5.5
@@ -38,7 +49,9 @@ typedef struct {
   char *custom_option;           /**< Custom option string for neural network framework. */
   char *fw_name;                 /**< The explicit framework name given by user */
   int invoke_dynamic;            /**< True for supporting invoke with flexible output. */
-  int invoke_async;              /**< The sub-plugin must support asynchronous output to use this option. If set to TRUE, the sub-plugin can generate multiple outputs asynchronously per single input. Otherwise, only synchronous single-output is expected and async callback/handle are ignored. */
+  int invoke_async;              /**< The sub-plugin must support asynchronous output to use this option. If set to TRUE, the sub-plugin can generate multiple outputs asynchronously per single input. Otherwise, only synchronous single-output is expected and async callback is ignored. */
+  ml_tensors_data_cb invoke_async_cb; /**< Callback function to be called when the sub-plugin generates an output asynchronously. This is only available when invoke_async is set to TRUE. */
+  void *invoke_async_pdata;           /**< Private data to be passed to async callback. */
 } ml_single_preset;
 
 /**
